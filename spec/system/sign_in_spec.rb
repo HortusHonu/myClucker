489b8842246e753855eb4bed8af79b8d131e25c1
Eric <james.eric.knight@gmail.com>
2020-05-03 15:32:48 -0400

    Cleaned up sign_in_spec

--- spec/system/sign_in_spec.rb
+++ spec/system/sign_in_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Sign in', type: :system do
-	it 'works' do
+	it 'works with default user' do
 		user = FactoryBot.create(:user)
 		visit new_user_session_path	
 		fill_in 'Email', with: user.email
 		fill_in 'Password', with: user.password
 		click_on 'Log in'
-		# expect "Sign out" link
 		expect(page).to have_content('Sign out')
 	end
 end
