1daa41cac6529f7a32a4be7d63bc239ffd3074ae
Eric <james.eric.knight@gmail.com>
2020-05-08 11:28:30 -0400

    Add test for trying to create an invalid Cluck

--- spec/system/create_cluck_spec.rb
+++ spec/system/create_cluck_spec.rb
@@ -1,13 +1,26 @@
-require 'rails_helper'
+require 'rails_helper'	
 
 RSpec.describe 'Creating a Cluck', type: :system do
-	context 'works' do
-		it 'lets the user create a cluck' do
-			login_as(FactoryBot.create(:user))
+	before do
+		login_as(FactoryBot.create(:user))
+	end
+
+	context 'valid cluck' do
+		it 'works' do
 			visit new_cluck_path
 			fill_in 'Content', with: "Hello"
 			click_on 'Create Cluck'
-			expect(page).to have_content 'Hello'
+			expect(page).to have_content('Hello')
 		end	
 	end
+
+	context 'blank' do
+		it 'gives a validation error' do
+			visit new_cluck_path
+			fill_in 'Content', with: ''
+			click_on 'Create Cluck'
+			expect(page).to have_content("Content can't be blank")
+		end	
+	end
+
 end
